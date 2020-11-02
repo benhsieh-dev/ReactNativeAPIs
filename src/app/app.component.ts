e855fcc25261790c575695fcce9bc0d2361804bd
Ben Hsieh <bqh5026@gmail.com>
2020-11-01 22:45:37 -0500

    Pagination app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'pages';
+  array = [
+    {
+      title: 'At the Beach',
+      url: 'https://images.unsplash.com/photo-1495954484750-af469f2f9be5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60' 
+  },
+    {
+      title: 'Vacation',
+      url: 'https://images.unsplash.com/photo-1527179528411-4219e0714bcc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60' 
+  },
+    {
+      title: 'New York City',
+      url: 'https://images.unsplash.com/photo-1579961861832-6fecf4b86d1f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60' 
+  },
+    {
+      title: 'Park',
+      url: 'https://images.unsplash.com/photo-1596443840057-85d1e0b7976b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60' 
+  },
+
+  ]
 }
