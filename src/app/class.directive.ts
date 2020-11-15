e2b3cc97f77d81b4c6a34d546e1ee6e21bc28fd2
benhsieh-dev <bqh5026@gmail.com>
2020-11-15 00:14:41 -0500

    Add input aliasing for class directives of background color

--- src/app/class.directive.ts
+++ src/app/class.directive.ts
@@ -1,12 +1,14 @@
-import { Directive, Input } from '@angular/core';
+import { Directive, ElementRef, Input } from '@angular/core';
 
 @Directive({
   selector: '[appClass]'
 })
 export class ClassDirective {
-  @Input() backgroundColor: string; 
-  constructor() { 
-      console.log('Class Directive Used');
+
+  constructor(private element: ElementRef) {     
   }
 
+  @Input('appClass') set backgroundColor(color: string) {
+      this.element.nativeElement.style.backgroundColor = color;
+    }
 }
