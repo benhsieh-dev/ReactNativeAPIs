ec08145e733ca3f6af38fd5bfae4b62373388b7e
benhsieh-dev <bqh5026@gmail.com>
2020-11-14 23:58:47 -0500

    Add class directive of background color to h4 element in app.component.html

---
+++ src/app/class.directive.ts
@@ -0,0 +1,12 @@
+import { Directive, Input } from '@angular/core';
+
+@Directive({
+  selector: '[appClass]'
+})
+export class ClassDirective {
+  @Input() backgroundColor: string; 
+  constructor() { 
+      console.log('Class Directive Used');
+  }
+
+}
