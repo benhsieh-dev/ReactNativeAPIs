af2c5a297434dfec3b7411e3b4684ae82eed29e4
Ben Hsieh <bqh5026@gmail.com>
2023-02-22 23:28:33 -0600

    Update styles

--- App.js
+++ App.js
@@ -6,11 +6,24 @@ const movieURL = "https://reactnative.dev/movies.json";
 const App = () => {
   const [isLoading, setLoading] = useState(true);
   const [data, setData] = useState([]);  
+  const [title, setTitle] = useState([]);
+  const [description, setDescription] = useState([]);
+  // const [response, setResponse] = useState();
+
+  // const debugging = () => {
+  //   console.log(response);
+  //   return <Text>Data</Text>
+  // }
 
     useEffect(() => {
     fetch(movieURL)
       .then(response => response.json())
-      .then((json) => setData(json.movies))
+      .then((json) => {
+        setData(json.movies);
+        setTitle(json.title); 
+        setDescription(json.description);
+        // setResponse(json);
+      })
       .catch((error) => alert(error))
       .finally(setLoading(false))            
   },[])
@@ -21,18 +34,22 @@ const App = () => {
       {isLoading ? 
         (<ActivityIndicator /> )
         :
-        ( <FlatList 
-          data={data}
-          keyExtractor={({ id }, index) => id}
-          renderItem={({ item }) => (
-            <Text>
+        ( 
+          <View>
+             <Text style={styles.title}>{title}</Text>
+             <FlatList 
+              data={data}
+              keyExtractor={({ id }, index) => id}
+              renderItem={({ item }) => (
+              <Text style={styles.movieText}>
               {item.title}
               {item.releaseYear}
-            </Text>
-          )}
-          
-          
+              </Text>
+          )}      
           />
+          <Text style={styles.description}>{description}</Text>
+          </View>
+       
           )}     
     </SafeAreaView>
   )
@@ -41,10 +58,28 @@ const App = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    // backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    // justifyContent: 'center',
+    marginTop: 48
   },
+  movieText: {
+    fontSize: 26,
+    fontWeight: "200"
+  },
+  title: {
+    fontSize: 32,
+    fontWeight: "bold"
+  },
+  description: {
+    textAlign: "center",
+    marginBottom: 18,
+    // fontSize: 26,
+    fontWeight: "200",
+    color: "green"
+  },
+
+
 });
 
 export default App; 
